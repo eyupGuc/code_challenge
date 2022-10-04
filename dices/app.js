e8e50b2acfb7299a5d6e684492e95f917d14a973
eyupGuc <meydanguresi@gmail.com>
2022-10-04 14:01:36 +0300

    random

--- dices/app.js
+++ dices/app.js
@@ -1,2 +1,10 @@
-let dices = [1, 2, 3, 4, 5, 6];
-const toDice = () => {};
+// let dices = [1, 2, 3, 4, 5, 6];
+// const toDice = (n1, n2) => {};
+
+// console.log(Math.floor(Math.random() * 7));
+let arr = [];
+let randomFon = (x) => {
+  for (let i = 1; i <= x; i++) {
+    arr.push(Math.floor(Math.random() * 7));
+  }
+};
