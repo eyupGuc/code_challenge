0169f47ec641ffa14ae7c23f86d89bfc232254cc
eyupGuc <meydanguresi@gmail.com>
2022-10-07 08:43:11 +0300

    number

--- password/app.js
+++ password/app.js
@@ -3,8 +3,14 @@ const scope='!@#$%^&*()\_+~|}{[]:;?><,./-=';
 
 let passwordGenerator=(x)=>{
     //** rule 1 */
+    let number=/[0-9]/;
+    let a =x.search(number);
+    console.log(number)
+    
     if(x.length!==10){
         return console.log("password must be at least ten characters")
     }
+   
+  
 }
 passwordGenerator(password);
