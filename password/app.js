ed4fcf7b44b9284526ebc45514628ee9d3a5e930
eyupGuc <meydanguresi@gmail.com>
2022-10-07 10:33:26 +0300

    move

--- password/app.js
+++ password/app.js
@@ -1,15 +1,19 @@
-const password="!@H5xy324";
+const password="!@Hxyrty23";
 const scope='!@#$%^&*()\_+~|}{[]:;?><,./-=';
 
 let passwordGenerator=(x)=>{
     //** rule 1 */
-    let number=/[0-9]/;
-    let a =x.search(number);
-    console.log(a)
+   
     
     if(x.length!==10){
         return console.log("password must be at least ten characters")
     }
+
+    let number=/[0-9]/;
+    let a =x.search(number);
+    // console.log(a)
+    let b=x.slice( a+1).search(number)
+    console.log(b)
    
   
 }
